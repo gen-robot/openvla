33308118bece4f0e0f7ef7ea2fb23e586432a623
slzhta <1207221042@qq.com>
2025-05-12 21:05:02 +0800

    add is_correction in rlds and test correction data

--- lora_cot.sh
+++ lora_cot.sh
@@ -16,24 +16,28 @@ is_debug=False
 enable_cot=True
 use_lora=True
 cot_full=$1
-cot_tags="task"
+resume=$2
+vla_path=$3
+resume_step=$4
+cot_tags="move_reason,move"
 
 # if is_debug is True, set num_gpus to 1, set project name to OpenVLA-debug
 if [ ${is_debug} = True ]; then
     num_gpus=1
     project_name="OpenVLA-debug"
 elif [ ${enable_cot} = True ]; then
-    num_gpus=4
+    num_gpus=2
     project_name="VLA-Reasoning"
 else
-    num_gpus=4
+    num_gpus=2
     project_name="VLA-Reasoning"
 fi
 # openvla-ecot/ecot-openvla-7b-oxe \
+    # --vla_path /nvme_data/liangzhi/pretrained/openvla-7b/ \
 
 if [ ${cot_full} = True ]; then
   torchrun --standalone --nnodes 1 --nproc-per-node ${num_gpus} vla-scripts/finetune.py \
-    --vla_path /nvme_data/liangzhi/pretrained/ecot-openvla-7b-oxe/ \
+    --vla_path ${vla_path} \
     --data_root_dir datasets/libero_data \
     --dataset_name ${task_name} \
     --run_root_dir checkpoints/${task_name} \
@@ -42,35 +46,8 @@ if [ ${cot_full} = True ]; then
     --num_images_in_input ${num_images_in_input} \
     --use_lora ${use_lora} \
     --lora_rank 32 \
-    --batch_size 1 \
-    --grad_accumulation_steps 4 \
-    --learning_rate 5e-4 \
-    --image_aug False \
-    --wandb_project ${project_name} \
-    --max_steps 100_000 \
-    --merge_lora_during_training ${merge_lora_during_training} \
-    --use_l1_regression ${use_l1_regression} \
-    --use_diffusion ${use_diffusion} \
-    --use_parallel_decoding ${use_parallel_decoding} \
-    --enable_cot ${enable_cot} \
-    --num_actions_chunk ${num_actions_chunk} \
-    --use_val_set True \
-    --save_freq 5000 \
-    --val_freq 1000 
-  #   --cot_tags ${cot_tags}
-else
-  torchrun --standalone --nnodes 1 --nproc-per-node ${num_gpus} vla-scripts/finetune.py \
-    --vla_path /nvme_data/liangzhi/pretrained/ecot-openvla-7b-oxe/ \
-    --data_root_dir datasets/libero_data \
-    --dataset_name ${task_name} \
-    --run_root_dir checkpoints/${task_name} \
-    --use_proprio ${use_proprio} \
-    --use_film ${use_film} \
-    --num_images_in_input ${num_images_in_input} \
-    --use_lora ${use_lora} \
-    --lora_rank 32 \
-    --batch_size 1 \
-    --grad_accumulation_steps 4 \
+    --batch_size 4 \
+    --grad_accumulation_steps 2 \
     --learning_rate 5e-4 \
     --image_aug False \
     --wandb_project ${project_name} \
@@ -84,5 +61,36 @@ else
     --use_val_set True \
     --save_freq 5000 \
     --val_freq 1000 \
-    --cot_tags ${cot_tags}
+    --resume ${resume} \
+    --resume_step ${resume_step}
+  #   --cot_tags ${cot_tags}
+else
+  torchrun --standalone --nnodes 1 --nproc-per-node ${num_gpus} vla-scripts/finetune.py \
+    --vla_path ${vla_path} \
+    --data_root_dir datasets/libero_data \
+    --dataset_name ${task_name} \
+    --run_root_dir checkpoints/${task_name} \
+    --use_proprio ${use_proprio} \
+    --use_film ${use_film} \
+    --num_images_in_input ${num_images_in_input} \
+    --use_lora ${use_lora} \
+    --lora_rank 32 \
+    --batch_size 4 \
+    --grad_accumulation_steps 2 \
+    --learning_rate 5e-4 \
+    --image_aug False \
+    --wandb_project ${project_name} \
+    --max_steps 100_000 \
+    --merge_lora_during_training ${merge_lora_during_training} \
+    --use_l1_regression ${use_l1_regression} \
+    --use_diffusion ${use_diffusion} \
+    --use_parallel_decoding ${use_parallel_decoding} \
+    --enable_cot ${enable_cot} \
+    --num_actions_chunk ${num_actions_chunk} \
+    --use_val_set True \
+    --save_freq 5000 \
+    --val_freq 1000 \
+    --cot_tags ${cot_tags} \
+    --resume ${resume} \
+    --resume_step ${resume_step}
 fi
--- prismatic/vla/datasets/rlds/oxe/transforms.py
+++ prismatic/vla/datasets/rlds/oxe/transforms.py
@@ -933,6 +933,7 @@ OXE_STANDARDIZATION_TRANSFORMS = {
     "libero_object_no_noops": libero_dataset_transform,
     "libero_goal_no_noops": libero_dataset_transform,
     "libero_10_no_noops": libero_dataset_transform,
+    "libero_correction_object_no_noops": libero_dataset_transform,
     ### Cobot
     "cobot_rlds_dataset": cobot_dataset_transform,
     "cobot_future_dataset": cobot_dataset_transform,
